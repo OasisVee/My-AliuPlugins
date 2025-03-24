6c9931df0c9c248c58298a576a76470949947e8e
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-24 11:39:52 -0400

    Create build.gradle.kts

---
+++ PlusReact/build.gradle.kts
@@ -0,0 +1,7 @@
+version = "1.0.0"
+description = "backport of the +:[emote] reaction functionality with functionality to go up to 5 messages up to react using more pluses"
+
+aliucord.changelog.set("""
+    # 1.0.0
+    * inital release
+""".trimIndent())
